cca97fe03ad0f184b1f65632db561987e80a8439
Princessb666 <princessboro106@gmail.com>
2025-10-28 20:59:14 +0530

    Add files via upload

---
+++ assignment8.js
@@ -0,0 +1,38 @@
+// Set the date we're counting down to (Example: New Year 2026)
+const countdownDate = new Date("Jan 1, 2026 00:00:00").getTime();
+
+const timerElement = document.getElementById('timer');
+const messageElement = document.getElementById('message');
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countdownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the corresponding elements
+    daysElement.textContent = String(days).padStart(2, '0');
+    hoursElement.textContent = String(hours).padStart(2, '0');
+    minutesElement.textContent = String(minutes).padStart(2, '0');
+    secondsElement.textContent = String(seconds).padStart(2, '0');
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        timerElement.classList.add('hidden');
+        messageElement.classList.remove('hidden');
+    }
+}, 1000);
